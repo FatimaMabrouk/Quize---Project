fb80499ffc6b46f7cc2e02865b0c1e89a21dd2b0
fatima mabrouk <onlyfefe03@gmail.com>
2019-11-07 16:31:24 +0100

    thired

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+document.getElementById('next').onclick = function quizPage(){
+   document.getElementById('next') = '<a herf='quize.html'> </a>'
+} 
+
+// quize 
+// store qustion on array // 
+var qustion = ['q1','q2','q3','q4','q5','q6','q7','q8','q9','q1']
--- jquery.js
+++ jquery.js
@@ -1,2 +1,21 @@
-// check jquery is linked or not
- $('body').css('background-color','blue')
+// // check jquery is linked or not
+//  $('body').css('background-color','blue');
+//  // $(document).ready(function(){
+//  // 	$('#next').on('click', function(){
+
+//  // 	})
+//  // })
+$(document).ready(function(){
+
+var $body = $('body');
+$('form').submit(function(){
+ $('body').hide();
+ $('.form2').appendTo('body');	
+})
+
+
+
+
+
+
+})
