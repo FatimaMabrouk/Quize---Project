51fe027234f7e3eee6e18ba23a9702cf5e86a086
fatima mabrouk <onlyfefe03@gmail.com>
2019-11-07 22:18:44 +0100

    commit 6

--- javaScript.js
+++ javaScript.js
@@ -7,20 +7,68 @@ var qustions = [{
   "Choice4": "Choices 1 and 3 but not 2",
   "answer": " "
  }, {
-  "qustion":"According to your text, one study found that 90 percent of firings result from ?",
-  "Choice1": "interpersonal relationship problems.",
-  "Choice2": "substandard interpersonal skills.",
-  "Choice3": "inappropriate behavior.",
-  "Choice4": "Choices 1 and 3 but not 2",
+  "qustion":"Which of the following is interpersonal skill training?",
+  "Choice1": "Basic skills",
+  "Choice2": "Intermediate skills",
+  "Choice3": "Soft skills.",
+  "Choice4": "Hard skills",
   "answer": " "
 
   },
   {
-  "qustion":"According to your text, one study found that 90 percent of firings result from ?",
-  "Choice1": "interpersonal relationship problems.",
-  "Choice2": "substandard interpersonal skills.",
-  "Choice3": "inappropriate behavior.",
-  "Choice4": "Choices 1 and 3 but not 2",
+  "qustion":"What is an action plan?",
+  "Choice1": "A dream",
+  "Choice2": "A set of steps to be followed to reach a goal",
+  "Choice3": "A good credit rating",
+  "Choice4": "A system to beat the odds",
   "answer": " "
-  },
+  }, {
+  "qustion":"Which step immediately follows implementing an action plan?",
+  "Choice1": "Be cooperative and attentive",
+  "Choice2": "Determine action steps",
+  "Choice3": "Set goals",
+  "Choice4": "Measure the effects of one's actions against reality",
+  "answer": " "
+
+  }, {
+  "qustion":"What is true of complex skill-improvement goals?",
+  "Choice1": "Results should be measured by one simple measure to avoid inconsistency.",
+  "Choice2": "Progress should be measured in several ways.",
+  "Choice3": "The goal is probably not realistic",
+  "Choice4": "Goals should never be complex",
+  "answer": " "
+  }, {
+  "qustion": "What is the final step in the learning model?",
+  "Choice1": "Fine tune the behavior",
+  "Choice2": "Integrate the behavior into your habitual or usual way of behaving",
+  "Choice3": "Evaluate the effects of the individual steps",
+  "Choice4": "Compare the effects to the plan",
+  "answer": " "
+}, {
+  "qustion":"What do we call a specific area in which a person needs to change",
+  "Choice1": "Psychological need",
+  "Choice2": "Developmental need",
+  "Choice3": "Behavioral need",
+  "Choice4": "Social need",
+  "answer": " "
+}, {
+  "qustion":"Which employment tool might give you very useful feedback about your interpersonal skills?",
+  "Choice1": "Performance evaluation",
+  "Choice2": "Safety checklist",
+  "Choice3": "Department budget",
+  "Choice4": "Policy and procedures manual",
+  "answer": " "
+}
+
+
+
+
+
+
+
+
+
+
+
+
 ]
