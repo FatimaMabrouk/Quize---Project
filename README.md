ab5b7edb4a6bfbf449392033b24fdd35d0856214
fatima mabrouk <onlyfefe03@gmail.com>
2019-11-09 06:29:01 +0100

    14

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Quize---Project
+# TIC Toc ( X AND O) game.
+its for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in 
+- macke a game to play with computer wich you choose the turn you want the the computer will but valus by defful ..
--- tictac.js
+++ tictac.js
@@ -1,6 +1,11 @@
 
 // alert('checked');
 $(document).ready(function (){ 
+$(".tic").text('');
+$('button').on('click', function(){
+  $(this).fadeOut(100).fadeIn(120);
+});
+
 
 var turn='X';
 
@@ -18,6 +23,7 @@ var count= 0;
 // change player's turn to x and computers to O
 $('#turnX').on('click', function(){
   turn = 'O';
+  $(".tic").text('');
   computersTurn = 'X';
   $('#turnX').removeClass("player");
   $('#turnO').addClass("player");
@@ -27,6 +33,7 @@ $('#turnX').on('click', function(){
 
 $('#turnO').on('click', function(){
   turn = 'X';
+  $(".tic").text('');
   computersTurn = 'O';
   $('#turnO').removeClass("player first");
   $('#turnX').addClass("player seconde");
@@ -42,15 +49,15 @@ function computerTurn(){
   // genarate random
     var computersMove = (Math.random()*10).toFixed();
     var  move = $('#'+computersMove).text();
-   if(move ==="#") {
-      $("#"+computersMove).text(computersTurn);
-       taken = true;
-       turns[computersMove] = computersTurn;
-        }
+    if(move ==="#") {
+       $("#"+computersMove).text(computersTurn);
+        taken = true;
+        turns[computersMove] = computersTurn;
+         }
 
       }
 
-    }
+  }
 
 
 /////////// turn ////////////
@@ -77,13 +84,15 @@ function winCondition(turnArray, currentTurn) {
   if(turnArray[0] === currentTurn && turnArray[1] === currentTurn && turnArray[2] === currentTurn){
      gameCon = true;
      reset();
-     alert("player" + currentTurn + " wins! (Top row across 0,1, and 2 spots)");
+      alert("player" + currentTurn + " wins! (Top row across 0,1, and 2 spots)");
+    
     }
   // 2
   else if(turnArray[2]=== currentTurn && turnArray[4] === currentTurn && turnArray[6]=== currentTurn ){
       gameCon = true;
       reset();
       alert("player" + currentTurn + " wins! (Top row across 2,4, and 6 spots)" );
+
   }
   // 3 
   else if(turnArray[0] === currentTurn && turnArray[3] === currentTurn && turnArray[6] === currentTurn) {
